d66caef3b80f535725a3e0962f0e28eca1576ed2
Nicolas Garcia <n@beu.app>
2026-01-20 14:39:40 -0500

    feat(10-01): add contract components barrel export
    - Export ContractTimeline, ContractPreview, SignatureForm
    - Centralized import point for contract components

---
+++ src/components/contract/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Contract components barrel export
+ */
+
+export { ContractTimeline } from './ContractTimeline';
+export { ContractPreview } from './ContractPreview';
+export { SignatureForm } from './SignatureForm';
